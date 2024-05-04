4a49c2baa6ada6758ac11d3719c6b2bee0de7bc1
YummYume <yummyume@users.noreply.github.com>
2024-05-04 18:46:42 +0200

    Try to implement gorm custom data type

--- api/enum/stratagem_keys.go
+++ api/enum/stratagem_keys.go
@@ -1,5 +1,10 @@
 package enum
 
+import (
+	"database/sql/driver"
+	"fmt"
+)
+
 type StratagemKeys string
 
 const (
@@ -8,3 +13,28 @@ const (
 	Down  StratagemKeys = "down"
 	Left  StratagemKeys = "left"
 )
+
+func (stratagemKey *StratagemKeys) Scan(value interface{}) error {
+	if value == nil {
+		return nil
+	}
+
+	switch value {
+	case "up":
+		*stratagemKey = Up
+	case "right":
+		*stratagemKey = Right
+	case "down":
+		*stratagemKey = Down
+	case "left":
+		*stratagemKey = Left
+	default:
+		return fmt.Errorf("invalid value for StratagemKeys: %v", value)
+	}
+
+	return nil
+}
+
+func (stratagemKey StratagemKeys) Value() (driver.Value, error) {
+	return string(stratagemKey), nil
+}
