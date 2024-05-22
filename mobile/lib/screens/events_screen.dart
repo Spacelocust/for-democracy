20cb9bbbb8bca549b557b9194d7a5239a9d1566e
LeoBruant <bruantleo@gmail.com>
2024-05-22 23:26:38 +0200

    WIP events screen
    Signed-off-by: LeoBruant <bruantleo@gmail.com>

--- mobile/lib/screens/events_screen.dart
+++ mobile/lib/screens/events_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class EventsScreen extends StatefulWidget {
   static const String routePath = '/events';
@@ -12,10 +11,103 @@ class EventsScreen extends StatefulWidget {
 }
 
 class _EventsScreenState extends State<EventsScreen> {
+  final List<dynamic> defences = [
+    {
+      "health": 69,
+      "planet": {
+        "maxHealth": 420,
+        "name": "Super Earth",
+      },
+    },
+    {
+      "health": 69,
+      "planet": {
+        "maxHealth": 420,
+        "name": "Super Earth",
+      },
+    }
+  ];
+
+  final List<dynamic> liberation = [
+    {
+      "health": 69,
+      "planet": {
+        "maxHealth": 420,
+        "name": "Big Earth",
+      },
+    },
+    {
+      "health": 69,
+      "planet": {
+        "maxHealth": 420,
+        "name": "Big Earth",
+      },
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(AppLocalizations.of(context)!.events),
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          // Liberations
+          const Text(
+            "Ongoing Liberations",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: liberation.length,
+            itemBuilder: (context, index) {
+              final liberationItem = liberation[index];
+
+              return ListTile(
+                title: Text(liberationItem['planet']['name']),
+                subtitle: Text("Health: ${liberationItem['health']}"),
+              );
+            },
+          ),
+
+          const SizedBox(height: 16),
+
+          // Defences
+          const Text(
+            "Ongoing Defences",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          ListView.builder(
+            itemCount: defences.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              final defenceItem = defences[index];
+
+              return Column(
+                children: [
+                  const Text(
+                    "Victory in 420H 42M, 69 hours left",
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  ListTile(
+                    title: Text(defenceItem['planet']['name']),
+                    subtitle: Text(
+                        "${(defenceItem['health'] * 100 / defenceItem['planet']['maxHealth']).toStringAsFixed(2)}% liberated"),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
