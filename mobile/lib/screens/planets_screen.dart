a56b9e251253aeac72ec8d75d88a4c82d0ab25e6
YummYume <yummyume@users.noreply.github.com>
2024-07-08 16:54:18 +0200

    Fix CI

--- mobile/lib/screens/planets_screen.dart
+++ mobile/lib/screens/planets_screen.dart
@@ -1,6 +1,3 @@
-import 'dart:developer';
-
-import 'package:eventflux/eventflux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mobile/models/planet.dart';
