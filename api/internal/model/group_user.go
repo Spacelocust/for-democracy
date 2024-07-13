349dbd3cd4dfd3515e4e43d4170f0308a024ba21
LeoBruant <bruantleo@gmail.com>
2024-07-13 16:15:28 +0200

    Fix : Migrations
    Signed-off-by: LeoBruant <bruantleo@gmail.com>

--- api/internal/model/group_user.go
+++ api/internal/model/group_user.go
@@ -6,8 +6,14 @@ import (
 
 type GroupUser struct {
 	gorm.Model
-	GroupID uint
-	User    User
-	UserID  uint
-	Owner   bool `gorm:"not null;default:false"`
+	GroupID           uint
+	User              User
+	UserID  	      uint
+	Owner      		  bool               `gorm:"not null;default:false"`
+	GroupUserMissions []GroupUserMission `gorm:"constraint:OnDelete:CASCADE"`
+}
+
+func (gu *GroupUser) BeforeDelete(tx *gorm.DB) (err error) {
+    // Delete all GroupUserMission records associated with the GroupUser
+    return tx.Unscoped().Where("group_user_id = ?", gu.ID).Delete(&GroupUserMission{}).Error
 }
