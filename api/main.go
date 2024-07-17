aa9ae4597cdf0ee78855d57905cba0ee11c77bb8
spacelocust <39099403+Spacelocust@users.noreply.github.com>
2024-07-17 15:44:18 +0200

    fix: swagger

--- api/main.go
+++ api/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	_ "ariga.io/atlas-provider-gorm/gormschema"
 	"github.com/Spacelocust/for-democracy/cmd"
-	"github.com/Spacelocust/for-democracy/docs"
 )
 
 // @BasePath /
@@ -19,15 +15,5 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
-
-	// Set up the Swagger documentation
-	if os.Getenv("API_ENV") == "production" {
-		docs.SwaggerInfo.Host = os.Getenv("API_DOMAIN")
-		docs.SwaggerInfo.Schemes = []string{"https"}
-	} else {
-		docs.SwaggerInfo.Host = fmt.Sprintf("%s:%s", os.Getenv("API_DOMAIN"), os.Getenv("API_PORT"))
-		docs.SwaggerInfo.Schemes = []string{"http"}
-	}
-
 	cmd.RunCLI()
 }
